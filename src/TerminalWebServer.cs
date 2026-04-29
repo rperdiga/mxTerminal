65c3b497d6bc0836ef7d050cbd8544c32395edcf
Joe <joe@lato-bicycles.com>
2026-04-29 19:06:39 -0400

    feat: WebServerExtension serving wwwroot/index.html + bundle

---
+++ src/TerminalWebServer.cs
@@ -0,0 +1,61 @@
+using System.ComponentModel.Composition;
+using System.Net;
+using Mendix.StudioPro.ExtensionsAPI.Services;
+using Mendix.StudioPro.ExtensionsAPI.UI.WebServer;
+
+namespace MxStudioProTerminal;
+
+[Export(typeof(WebServerExtension))]
+public sealed class TerminalWebServer : WebServerExtension
+{
+    private readonly IExtensionFileService extensionFileService;
+
+    [ImportingConstructor]
+    public TerminalWebServer(IExtensionFileService extensionFileService)
+    {
+        this.extensionFileService = extensionFileService;
+    }
+
+    public override void InitializeWebServer(IWebServer webServer)
+    {
+        webServer.AddRoute("index.html", ServeIndex);
+        webServer.AddRoute("terminal.bundle.js", ServeBundle);
+        webServer.AddRoute("terminal.bundle.js.map", ServeBundleMap);
+    }
+
+    private async Task ServeIndex(HttpListenerRequest request, HttpListenerResponse response, CancellationToken ct)
+    {
+        var path = extensionFileService.ResolvePath("wwwroot", "index.html");
+        await Serve(response, path, "text/html", ct);
+    }
+
+    private async Task ServeBundle(HttpListenerRequest request, HttpListenerResponse response, CancellationToken ct)
+    {
+        var path = extensionFileService.ResolvePath("wwwroot", "terminal.bundle.js");
+        await Serve(response, path, "text/javascript", ct);
+    }
+
+    private async Task ServeBundleMap(HttpListenerRequest request, HttpListenerResponse response, CancellationToken ct)
+    {
+        var path = extensionFileService.ResolvePath("wwwroot", "terminal.bundle.js.map");
+        await Serve(response, path, "application/json", ct);
+    }
+
+    private async Task Serve(HttpListenerResponse response, string path, string contentType, CancellationToken ct)
+    {
+        if (!File.Exists(path))
+        {
+            response.StatusCode = 404;
+            response.Close();
+            return;
+        }
+
+        var fileContents = await File.ReadAllBytesAsync(path, ct);
+        response.ContentType = contentType;
+        response.ContentLength64 = fileContents.Length;
+        response.StatusCode = 200;
+
+        await response.OutputStream.WriteAsync(fileContents, ct);
+        response.Close();
+    }
+}
