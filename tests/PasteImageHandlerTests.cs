1ef56ec61b9ef25576ba6bdc90488d678a7a21a7
Ricardo Perdigao <ricardo.perdigao@siemens.com>
2026-05-18 23:55:56 -0400

    test(paste-image): extension-from-MIME parameterized cases

--- tests/PasteImageHandlerTests.cs
+++ tests/PasteImageHandlerTests.cs
@@ -35,4 +35,31 @@ public class PasteImageHandlerTests : IDisposable
         File.Exists(path).Should().BeTrue();
         File.ReadAllBytes(path).Should().Equal(bytes);
     }
+
+    [Theory]
+    [InlineData("image/png",  ".png")]
+    [InlineData("image/jpeg", ".jpg")]
+    [InlineData("image/gif",  ".gif")]
+    [InlineData("image/webp", ".webp")]
+    [InlineData("image/bmp",  ".bmp")]
+    [InlineData("image/tiff", ".tiff")]
+    [InlineData("IMAGE/PNG",  ".png")]  // case-insensitive
+    public void WriteImage_PicksExtensionFromMime(string mime, string expectedExt)
+    {
+        var handler = new PasteImageHandler(baseDir);
+        var path = handler.WriteImage(mime, new byte[] { 1, 2, 3 }, nameHint: null);
+        Path.GetExtension(path).Should().Be(expectedExt);
+    }
+
+    [Theory]
+    [InlineData("image/avif")]
+    [InlineData("image/heic")]
+    [InlineData("")]
+    [InlineData("text/plain")]
+    public void WriteImage_FallsBackToPng_OnUnknownOrEmptyMime(string mime)
+    {
+        var handler = new PasteImageHandler(baseDir);
+        var path = handler.WriteImage(mime, new byte[] { 1, 2, 3 }, nameHint: null);
+        Path.GetExtension(path).Should().Be(".png");
+    }
 }
